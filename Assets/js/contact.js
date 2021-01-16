a2ad9e73d442b89cab1fbe958858f4e8d1ae2a30
Phoenix <73591532+KarrynS@users.noreply.github.com>
2021-01-16 20:27:51 +1100

    Adding animation for hr line

--- Assets/js/contact.js
+++ Assets/js/contact.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     var buttonGit = $("#gitHubBtn")
 
-    $("#btnGithub").hover(function() {
-        $(this).addClass("btnGithub");
-        $(this).append("<button href='https://github.com/KarrynS' target='_blank'>GitHub</button>")
+    //$("#btnGithub").hover(function() {
+    //    $(this).addClass("btnGithub");
+    //    $(this).append("<button href='https://github.com/KarrynS' target='_blank'>GitHub</button>")
         
-    })
+    //})
+
+    $(".animateOne").animate({left: "45%"});
+    $(".animateTwo").animate({right: "45%"});
 
 });
